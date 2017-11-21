cb9575a8a8e5c2edcdcc7d550731033565afb3fa
syfad <sunyfad@gmail.com>
2017-11-21 15:40:11 +0800

    添加openresty脚本

---
+++ install_openresty.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+import os
+import sys
+# import getopt
+# import filecmp
+
+if os.getuid() == 0:
+    pass
+else:
+    print "当前用户不是root，请切换为root"
+    sys.exit(1)
+
+nginx_pkg_url = 'https://openresty.org/download/openresty-1.9.15.1.tar.gz'
+pkg_name = 'openresty-1.9.15.1'
+install_dir = '/dmdata/server/openresty'
+
+work_path = os.getcwd()
+#print work_path
+
+def install_openresty():
+    print "####检查安装目录......"
+    if os.path.exists(install_dir):
+        print(install_dir+ "已存在openresty目录，请确认")
+        sys.exit(1)
+    print("####安装目录正常......")
+
+    print('####yum安装依赖包')
+    cmd = "yum install gcc gcc-c++ zlib zlib-devel openssl openssl-devel pcre-devel pcre automake autoconf libtool make -y"
+    res = os.system(cmd)
+    if res !=0:
+        print('yum安装失败，请检查环境')
+        sys.exit(1)
+
+    print('####下载安装包......')
+    cmd = 'wget ' + nginx_pkg_url
+    res = os.system(cmd)
+    if res !=0:
+        print('下载源码失败，请检查当前网络')
+        sys.exit(1)
+
+    print('####解压安装包......')
+    cmd = 'tar -zxvf' +pkg_name+'.tar.gz'
+    res = os.system(cmd)
+    if res !=0:
+        os.system('rm' +pkg_name+'.tar.gz')
+        print('解压失败，重新运行此脚本下载。')
+        sys.exit(1)
+
+    #os.chdir(pkg_name)
+    print('####编译安装openresty......')
+    cmd = 'cd  ' +pkg_name+ '&& ./configure --prefix=/dmdata/server/openresty --user=nginx --group=nginx --with-http_v2_module --with-http_sub_module --with-http_stub_status_module --with-luajit && make && make install'
+    res = os.system(cmd)
+    if res !=0:
+        os.system('rm -f ' +pkg_name+'.tar.gz')
+        os.system('rm -rf ' +pkg_name)
+        print('编译安装openresty失败，请检查安装相关依赖库......')
+        sys.exit(1)
+
+if __name__ == '__main__':
+    install_openresty()
