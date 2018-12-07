024c045bc5a9ad51858c56ebb9d951ac4c2cd781
sun_admin <sunyfad@gmail.com>
2018-12-07 18:01:09 +0800

    add

--- py/refresh_aliyun_cdn.py
+++ py/refresh_aliyun_cdn.py
@@ -5,7 +5,7 @@
 # @Disc    : support python 2.x and 3.x
 # 刷新aliyun CDN，使用前更改：文件或目录
 
-import sys,os
+import sys, os
 import urllib, urllib2
 import base64
 import hmac
@@ -21,6 +21,8 @@ import traceback
 access_key_id = '';
 access_key_secret = '';
 cdn_server_address = 'https://cdn.aliyuncs.com'
+
+
 # CONFIGFILE = os.getcwd() + './access_key.ini'
 # CONFIGSECTION = 'Credentials'
 
@@ -31,11 +33,12 @@ def percent_encode(str):
     res = res.replace('%7E', '~')
     return res
 
+
 def compute_signature(parameters, access_key_secret):
     sortedParameters = sorted(parameters.items(), key=lambda parameters: parameters[0])
 
     canonicalizedQueryString = ''
-    for (k,v) in sortedParameters:
+    for (k, v) in sortedParameters:
         canonicalizedQueryString += '&' + percent_encode(k) + '=' + percent_encode(v)
 
     stringToSign = 'GET&%2F&' + percent_encode(canonicalizedQueryString[1:])
@@ -44,17 +47,18 @@ def compute_signature(parameters, access_key_secret):
     signature = base64.encodestring(h.digest()).strip()
     return signature
 
+
 def compose_url(user_params):
     timestamp = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
 
-    parameters = { \
-            'Format'        : 'JSON', \
-            'Version'       : '2014-11-11', \
-            'AccessKeyId'   : access_key_id, \
-            'SignatureVersion'  : '1.0', \
-            'SignatureMethod'   : 'HMAC-SHA1', \
-            'SignatureNonce'    : str(uuid.uuid1()), \
-            'TimeStamp'         : timestamp, \
+    parameters = {
+        'Format': 'JSON',
+        'Version': '2014-11-11',
+        'AccessKeyId': access_key_id,
+        'SignatureVersion': '1.0',
+        'SignatureMethod': 'HMAC-SHA1',
+        'SignatureNonce': str(uuid.uuid1()),
+        'TimeStamp': timestamp,
     }
 
     for key in user_params.keys():
@@ -65,12 +69,14 @@ def compose_url(user_params):
     url = cdn_server_address + "/?" + urllib.urlencode(parameters)
     return url
 
+
 def make_request(Path, quiet=False):
-    #'ObjectType': 'Directory'
+    # 'ObjectType': 'Directory'
     obj = {'Action': 'RefreshObjectCaches', 'ObjectType': 'file', 'ObjectPath': 'http://yourdomain/1.txt'}
     obj['ObjectPath'] = Path
     url = compose_url(obj)
     return url
+
 
 if __name__ == '__main__':
     UrlPath = make_request(sys.argv[1])
