b9e74cfbaa0c5a0a95ce34a6afe53a6094a5ea8c
sun_admin <sunyfad@gmail.com>
2019-07-26 19:20:07 +0800

    add

---
+++ Ansible_model/ansible_api_files/new_api_module.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019
+# @Author  : SunYunfeng(sun_admin@126.com)
+# @Disc    : 
+# @Disc    : support python 2.x and 3.x
+
+#!/usr/bin/env python
+from collections import namedtuple
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars import VariableManager
+from ansible.inventory import Inventory
+from ansible.playbook.play import Play
+from ansible.executor.task_queue_manager import TaskQueueManager
+Options = namedtuple('Options', ['listtags', 'listtasks', 'listhosts', 'syntax', 'connection','module_path', 'forks', 'private_key_file', 'ssh_common_args', 'ssh_extra_args',
+'sftp_extra_args', 'scp_extra_args', 'become', 'become_method', 'become_user', 'verbosity', 'check'])
+variable_manager = VariableManager()
+loader = DataLoader()
+
+options = Options(listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None, forks=100, private_key_file=None,
+ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=False, become_method=None, become_user=None, verbosity=None, check=False)
+passwords = {}
+inventory = Inventory(loader=loader, variable_manager=variable_manager)
+variable_manager.set_inventory(inventory)
+
+
+
+play_source =  dict(
+        name = "Ansible Play",
+        hosts = 'localhost',
+        gather_facts = 'no',
+        tasks = [
+            dict(action=dict(module='shell', args='ls'), register='shell_out'),
+            dict(action=dict(module='echopong', args='pong'))
+         ]
+  )
+play = Play().load(play_source, variable_manager=variable_manager, loader=loader)
+qm = None
+try:
+    tqm = TaskQueueManager(
+              inventory=inventory,
+              variable_manager=variable_manager,
+              loader=loader,
+              options=options,
+              passwords=passwords,
+              stdout_callback='default',
+          )
+    result = tqm.run(play)
+finally:
+    if tqm is not None:
+        tqm.cleanup()
